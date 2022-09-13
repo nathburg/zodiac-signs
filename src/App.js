fdc9fe434d8678cb54a23677a5cbe6e66ce851cd
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-13 16:58:49 -0700

    MVP i think

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
